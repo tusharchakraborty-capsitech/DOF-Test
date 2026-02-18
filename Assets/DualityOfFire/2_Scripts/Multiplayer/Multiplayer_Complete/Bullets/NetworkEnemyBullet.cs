4838608cc7a23edaf1e8a96bcb5cb4196f0863fe
Tushar Chakraborty <tushar.chakraborty@capsitech.com>
2026-02-18 12:07:43 +0530

    remove unnesessary files

--- Assets/DualityOfFire/2_Scripts/Multiplayer/Multiplayer_Complete/Bullets/NetworkEnemyBullet.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-
-public class NetworkEnemyBullet : NetworkBulletController
-{
-    protected override string TargetTag => "Player";
-    protected override Vector2 MoveDirection => Vector2.left;
-}
